ac0fee85262d3f6163a57d963e741ef6b749fb5a
mihaibirsan28 <mitzabirsan12321@yahoo.com>
2021-12-28 18:39:59 +0200

    putin ca nu am apucat

---
+++ Entities/BestOf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proiect_daw.Entities
+{
+    public class BestOf
+    {
+        public int BestOfID { get; set; }
+        //[Required(ErrorMessage = "Categoria pentru top este obligatorie!")]
+        public string Category { get; set; }
+        public string CarList { get; set; }
+    }
+}
