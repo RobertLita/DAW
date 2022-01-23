17011463a039cc0ac814d4db8da077d95fdd583f
RobertLita <lita.robert@yahoo.com>
2022-01-24 00:09:27 +0200

    Logo added

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MarksPipe } from 'src/app/marks.pipe';
-//import { HoverBtnDirective } from 'src/app/hover-btn.directive';
+import { HoverBtnDirective } from 'src/app/hover-btn.directive';
 
 
 @NgModule({
   declarations: [MarksPipe,],
   imports: [
     CommonModule,
-    
+    HoverBtnDirective
   ],
   exports: [
     MarksPipe,
-
+    HoverBtnDirective
   ]
 })
 export class SharedModule { }
